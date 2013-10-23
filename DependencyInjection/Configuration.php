3e2024f1aa0ac1939811c5f75f6f635f1fca7b61
Mateusz Ryba <mr.mateuszryba@gmail.com>
2013-10-23 15:44:38 +0200

    Names corrected once again2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('dev_dash_otbs');
+        $rootNode = $treeBuilder->root('otbs');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
--- DependencyInjection/OTBSExtension.php
+++ DependencyInjection/OTBSExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class RybaOTBSExtension extends Extension
+class OTBSExtension extends Extension
 {
     /**
      * {@inheritDoc}
